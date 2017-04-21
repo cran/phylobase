5e662a830c605b02b5b2a42826b874f51fd4a826
Francois Michonneau <francois.michonneau@gmail.com>
2017-04-21 22:40:13 +0000

    version 0.8.4

--- vignettes/auto/phylobase.el
+++ vignettes/auto/phylobase.el
@@ -3,6 +3,10 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "article"
@@ -12,5 +16,6 @@
     "array"
     "url")
    (TeX-add-symbols
-    '("code" 1))))
+    '("code" 1)))
+ :latex)
 
